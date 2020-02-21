9e061c68269b96d3916b86a06d09f4ee1dec7a3f
Haris <harisq714@hotmail.com>
2020-02-20 23:36:01 -0600

    working on load_cached_devices function

--- shutter/shutter/controller/scripts/load_cached_devices.py
+++ shutter/shutter/controller/scripts/load_cached_devices.py
@@ -4,7 +4,6 @@ import os
 
 def load_cached_devices():
 	d = {}
-	t = {}
 	thing = open("/home/pi/Desktop/hub-repository/shutter/shutter/controller/scripts/known_devices.json",'r+')
 	temp = json.loads(thing.read())
 	thing.close()
--- shutter/shutter/controller/views.py
+++ shutter/shutter/controller/views.py
@@ -7,15 +7,19 @@ import os
 
 def load_cached_devices():
 	d = {}
-	t = {}
 	thing = open("/home/pi/Desktop/hub-repository/shutter/shutter/controller/scripts/known_devices.json",'r+')
 	temp = json.loads(thing.read())
-	print(temp)
-	#x = temp.values()
-	#print(x)
 	thing.close()
-	for x in temp.items:
-            print(x.keys)
+	name = temp['name']
+	mac = temp['mac_address']
+	i =0
+	while i < len(temp):
+		d['name'] = temp['name']
+		d['mac_address'] = temp['mac_address']
+		i+=1
+
+	return d
+	d = load_cached_devices()
 	return d
 
 known_devices = load_cached_devices()
