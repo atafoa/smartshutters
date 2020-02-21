7e7170ec96461c1feceb8f16a5695d4a11767b28
Haris Qureshi <harisq714@hotmail.com>
2020-02-20 22:46:25 -0600

    fixed the filepath

--- shutter/shutter/controller/scripts/load_cached_devices.py
+++ shutter/shutter/controller/scripts/load_cached_devices.py
@@ -5,7 +5,7 @@ import os
 def load_cached_devices():
 	d = {}
 	t = {}
-	thing = open("/home/pi/hub-repository/shutter/shutter/controller/scripts/known_devices.json",'r+')
+	thing = open("/home/pi/Desktop/hub-repository/shutter/shutter/controller/scripts/known_devices.json",'r+')
 	temp = json.loads(thing.read())
 	print(temp)
 	#x = temp.values()
