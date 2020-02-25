2b7a7c92475f399ccea3b888bbc2138742b7cfe0
haris <harisqureshi714@gmail.com>
2020-02-25 15:48:08 -0600

    transfferring to pie

--- shutter/shutter/controller/views.py
+++ shutter/shutter/controller/views.py
@@ -14,13 +14,13 @@ def load_cached_devices():
 	temp = json.loads(thing.read())
 	thing.close()
 	i =0
+	return d
 '''	while i < len(temp):
 		d['name'] = temp['name']
 		d['mac_address'] = temp['mac_address']
 		i+=1
 '''
 	#return d
-	return d
 
 known_devices = load_cached_devices()
 
