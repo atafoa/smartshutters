bab8cf6c11a267ac7c55df96bc76c11d2234f591
Haris Qureshi <harisq714@hotmail.com>
2020-02-25 16:24:06 -0600

    fixed line spcaing again

--- shutter/shutter/controller/scripts/load_cached_devices.py
+++ shutter/shutter/controller/scripts/load_cached_devices.py
@@ -18,8 +18,7 @@ def load_cached_devices():
 		d['name'] = temp['name']
 		d['mac_address'] = temp['mac_address']
 		i+=1
-	'''
-	testing tabs 
+	''' 
 	return d
 d = load_cached_devices()
 print (d)
