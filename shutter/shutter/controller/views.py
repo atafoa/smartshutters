cd4a496ce3668b04bd7a373c0da2e4f591d4b81e
Atafo Abure <atafoa@yahoo.com>
2020-02-27 17:25:07 -0600

    Getting ready for merge

--- shutter/shutter/controller/views.py
+++ shutter/shutter/controller/views.py
@@ -4,22 +4,17 @@ from django.http import HttpResponse
 import json
 import os
 import time
+import csv
 from . import ble_scan
 
 def load_cached_devices():
-	d = {}
-	t = {}
-	thing = open("/home/pi/hub-repository/shutter/shutter/controller/scripts/known_devices.json",'r+')
-	temp = json.loads(thing.read())
-	print(temp)
-	#x = temp.values()
-	#print(x)
-	thing.close()
-	'''
-	for x in temp.items:
-            print(x.keys)
-	'''
-	return d
+	thing={}
+	fp = open("/home/pi/Desktop/hub-repository/shutter/shutter/controller/scripts/knowndevs.csv", "r")
+	csvr = csv.DictReader(fp)
+	for row in csvr:
+		thing[row['name']]= row['mac']
+	print(thing)
+	return thing
 
 known_devices = load_cached_devices()
 
