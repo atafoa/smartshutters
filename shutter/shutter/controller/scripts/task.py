b6f87a5944396a967762cf4a042ed0c6f166ff30
haris qureshi <harisq714@hotmail.com>
2020-03-31 18:56:59 -0500

    added task.py to take bluetooth devices from terminal

---
+++ shutter/shutter/controller/scripts/task.py
@@ -0,0 +1,42 @@
+import sys
+import bluepy.btle as btle
+
+mac_address = sys.argv[1]
+position = sys.argv[2]
+print(mac_address,position)
+
+class MyDelegate(btle.DefaultDelegate):
+    
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+        
+    def handleNotification(self,cHandle,data):
+        
+        print(data)
+device = btle.Peripheral()
+device.setDelegate(MyDelegate())
+#device.connect("30:AE:A4:25:14:56")
+mac = str(mac_address)
+device.connect(sys.argv[1])
+services = device.getServices()
+print("Services discovered: ")
+print(services)
+characteristics = device.getCharacteristics()
+# for some reason characteristics[4] sends the write to the bluetooth module. Investigate later.
+print("Characteristics discovered:")
+for characteristics_single in characteristics:
+    print("UUID: " + str(characteristics_single.uuid))
+print("UUID for writing characteristic is:" + str(characteristics[4].uuid))
+characteristics[4].write(bytes("180"))
+
+"""
+while True:
+    if device.waitForNotifications(1.0):
+        print("Notification")
+        continue
+"""
+print("Waiting")
+device.disconnect()
+
+
+
