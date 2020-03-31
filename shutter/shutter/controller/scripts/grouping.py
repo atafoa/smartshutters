47fd11111ba794dc1e9330cbf85ab25e0a605536
haris qureshi <harisq714@hotmail.com>
2020-03-30 19:57:49 -0500

    working on the group functions

--- shutter/shutter/controller/scripts/grouping.py
+++ shutter/shutter/controller/scripts/grouping.py
@@ -4,31 +4,31 @@ temp = {"name":"mac_address"}
 room = input("enter the name of the room\t")
 name = input("enter name\t")
 mac = input("enter mac\t")
-group  = {}
+rooms  = {}
 
 #this is how you add to a room
-group[room] = temp
-print(group[room])
+rooms[room] = temp
+print(rooms[room])
 
 #this is how you pop from the room
-group[room].pop("name")
-print(group)
+rooms[room].pop("name")
+print(rooms)
 
 #this is how you add to a room
-group[room][name] = mac
-print(group[room])
+rooms[room][name] = mac
+print(room[room])
 
 #this is how you change the name of shutter in a certain room
-group[room]["new_name"] = group[room][name]
-group[room].pop(name)
-print(group)
+rooms[room]["new_name"] = rooms[room][name]
+rooms[room].pop(name)
+print(rooms)
 
 #this is how you can list the rooms 
-print(json.dumps(group))
+print(json.dumps(rooms))
 
 #this is how you can list devices in the room
-print(json.dumps(group[room]))
+print(json.dumps(rooms[room]))
 
 thing = {}
 #just dump every thing into a file and load from that file json should handle everything
-thing = json.loads(json.dumps(group))
+thing = json.loads(json.dumps(rooms))
