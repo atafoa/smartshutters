8117ba927930b852863743dfc56e1dbb054cdd69
haris qureshi <harisq714@hotmail.com>
2020-03-31 22:57:08 -0500

    almost done with cron just have to add finishing touches

---
+++ shutter/shutter/controller/scripts/set_time.py
@@ -0,0 +1,30 @@
+from crontab import CronTab
+import sys
+
+'''
+minute = sys.argv[1]
+hour = sys.argv[2]
+day_of_week = sys.argv[3]
+DOM = sys.argv[4]
+month = sys.argv[5]
+'''
+mac = sys.argv[1]
+position = sys.argv[2]
+
+cron = CronTab(user='pi')
+
+oof = 'python /home/pi/hub-repository/shutter/shutter/controller/scripts/task.py '+mac+' '+position
+#print(oof)
+
+job = cron.new(command = oof )
+job.minute.every(5)
+#job = cron.new(command='echo hello_world')
+job.enable()
+cron.write()
+for jobs in cron.lines:
+	print(jobs)
+	print("\n")
+#job.enable()
+#cron.remove_all()
+cron.remove(job)
+cron.write()
--- shutter/shutter/controller/scripts/task.py
+++ shutter/shutter/controller/scripts/task.py
@@ -3,7 +3,6 @@ import bluepy.btle as btle
 
 mac_address = sys.argv[1]
 position = sys.argv[2]
-print(mac_address,position)
 
 class MyDelegate(btle.DefaultDelegate):
     
@@ -11,23 +10,25 @@ class MyDelegate(btle.DefaultDelegate):
         btle.DefaultDelegate.__init__(self)
         
     def handleNotification(self,cHandle,data):
+		print(data)
         
-        print(data)
 device = btle.Peripheral()
 device.setDelegate(MyDelegate())
 #device.connect("30:AE:A4:25:14:56")
 mac = str(mac_address)
 device.connect(sys.argv[1])
 services = device.getServices()
-print("Services discovered: ")
+#print("Services discovered: ")
 print(services)
 characteristics = device.getCharacteristics()
 # for some reason characteristics[4] sends the write to the bluetooth module. Investigate later.
-print("Characteristics discovered:")
+#print("Characteristics discovered:")
+'''
 for characteristics_single in characteristics:
     print("UUID: " + str(characteristics_single.uuid))
-print("UUID for writing characteristic is:" + str(characteristics[4].uuid))
-characteristics[4].write(bytes("180"))
+#print("UUID for writing characteristic is:" + str(characteristics[4].uuid))
+'''
+characteristics[4].write(bytes(sys.argv[2]))
 
 """
 while True:
@@ -35,7 +36,7 @@ while True:
         print("Notification")
         continue
 """
-print("Waiting")
+#print("Waiting")
 device.disconnect()
 
 
