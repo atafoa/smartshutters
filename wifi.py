c3acad4eace9d882700dc5eb3c0e28c5698f8981
David Nguyen <dungnguyen1a9@gmail.com>
2020-04-02 20:49:55 +0000

    wifi file

---
+++ wifi.py
@@ -0,0 +1,27 @@
+# 1. install network-manager : sudo apt-get install network-manager
+# 2. run the script : sudo python try_network.py
+
+
+import os
+
+# open and append new wifi information in network file in root
+f = open("/etc/wpa_supplicant/wpa_supplicant.conf", "a")
+
+quotation = "\""
+### Input ID and password 
+wifi_id =  quotation + "Kate home" + quotation
+password = quotation + "Quang1292002?" + quotation
+protocal = "key_mgmt=WPA-PSK"
+
+
+f.write(
+"network={" + "\n" +
+"    ssid="+wifi_id+"\n"+
+"    psk="+password+"\n"+
+"    "+protocal+ "\n}"
+)
+
+f.close()
+
+# this command line will help the pi rescan and auto connect with the wifis
+os.system("wpa_cli -i wlan0 reconfigure")
